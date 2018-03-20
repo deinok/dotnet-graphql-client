ab89789eb7a4fd91c9c0739b47493d2d61cdbaba
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-03-20 11:08:33 +0100

    Working

--- dotnet-graphql-client/src/Program.cs
+++ dotnet-graphql-client/src/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.CommandLine;
+using Newtonsoft.Json;
 
-namespace dotnet_graphql_client
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace GraphQL.DotNet.GraphQL.Client {
+
+    class Program {
+
+        public static void Main(string[] args) {
+            args = new string[] { "-q", @"{person(personID:""1""){name}}" ,"-e", "https://swapi.apis.guru/", "-p" };
+
+            var endpoint = string.Empty;
+            var query = string.Empty;
+            var mode = Mode.POST;
+            var pretty = false;
+            var argumentSyntax=ArgumentSyntax.Parse(args, syntax =>{
+                {
+                    syntax.DefineOption("e|endpoint", ref endpoint, true, help: "The endpoint to target");
+                    if (!Uri.IsWellFormedUriString(endpoint, UriKind.RelativeOrAbsolute)) {
+                        syntax.ReportError("Not a valid endpoint");
+                    }
+                }
+                {
+                    syntax.DefineOption("q|query", ref query, true, help: "The GraphQL Query to send");
+                }
+                {
+                    syntax.DefineOption("m|mode", ref mode, (str) => { return Mode.POST; }, help: "The GraphQL Query to send");
+                }
+                {
+                    syntax.DefineOption("p|pretty", ref pretty, help: "JSON pretty");
+                }
+            });
+
+            using (var graphqlClient=new global::GraphQL.Client.GraphQLClient(endpoint)) {
+                var graphqlResponse= graphqlClient.PostQueryAsync(query).Result;
+                var jsonString=JsonConvert.SerializeObject(graphqlResponse, pretty ? Formatting.Indented : Formatting.None);
+                Console.Out.Write(jsonString);
+            }
+
         }
+
+        enum Mode {
+            GET,
+            POST
+        }
+
+
     }
+
 }
