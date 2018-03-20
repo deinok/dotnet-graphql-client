a557e6f0370108be4dbe892772f09e2cebf30660
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-03-20 10:05:48 +0100

    Initial Struct

---
+++ dotnet-graphql-client/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_graphql_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
