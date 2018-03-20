59f426f740edb3f4ccac9a6ae1575c89815f75f0
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-03-20 11:19:35 +0100

    pretty

--- src/dotnet-graphql-client/Program.cs
+++ src/dotnet-graphql-client/Program.cs
@@ -7,41 +7,32 @@ namespace GraphQL.DotNet.GraphQL.Client {
     class Program {
 
         public static void Main(string[] args) {
-            var endpoint = string.Empty;
-            var query = string.Empty;
-            var mode = Mode.POST;
-            var pretty = false;
-            var argumentSyntax=ArgumentSyntax.Parse(args, syntax =>{
+            var rawArguments = new RawArguments();
+            var argumentSyntax = ArgumentSyntax.Parse(args, syntax => {
                 {
-                    syntax.DefineOption("e|endpoint", ref endpoint, true, help: "The endpoint to target");
-                    if (!Uri.IsWellFormedUriString(endpoint, UriKind.RelativeOrAbsolute)) {
+                    syntax.DefineOption("e|endpoint", ref rawArguments.EndPoint, true, help: "The endpoint to target");
+                    if (!Uri.IsWellFormedUriString(rawArguments.EndPoint, UriKind.RelativeOrAbsolute)) {
                         syntax.ReportError("Not a valid endpoint");
                     }
                 }
                 {
-                    syntax.DefineOption("q|query", ref query, true, help: "The GraphQL Query to send");
+                    syntax.DefineOption("q|query", ref rawArguments.Query, true, help: "The GraphQL Query to send");
                 }
                 {
-                    syntax.DefineOption("m|mode", ref mode, (str) => { return Mode.POST; }, help: "The Mode to send the GraphQL");
+                    syntax.DefineOption("m|mode", ref rawArguments.Mode, (str) => { return RawArguments.Modee.POST; }, help: "The Mode to send the GraphQL");
                 }
                 {
-                    syntax.DefineOption("p|pretty", ref pretty, help: "JSON pretty");
+                    syntax.DefineOption("p|pretty", ref rawArguments.Pretty, help: "JSON pretty");
                 }
             });
 
-            using (var graphqlClient=new global::GraphQL.Client.GraphQLClient(endpoint)) {
-                var graphqlResponse= graphqlClient.PostQueryAsync(query).Result;
-                var jsonString=JsonConvert.SerializeObject(graphqlResponse, pretty ? Formatting.Indented : Formatting.None);
+            using (var graphqlClient = new global::GraphQL.Client.GraphQLClient(rawArguments.EndPoint)) {
+                var graphqlResponse = graphqlClient.PostQueryAsync(rawArguments.Query).Result;
+                var jsonString = JsonConvert.SerializeObject(graphqlResponse, rawArguments.Pretty ? Formatting.Indented : Formatting.None);
                 Console.Out.Write(jsonString);
             }
 
         }
-
-        enum Mode {
-            GET,
-            POST
-        }
-
 
     }
 
---
+++ src/dotnet-graphql-client/RawArguments.cs
@@ -0,0 +1,20 @@
+﻿namespace GraphQL.DotNet.GraphQL.Client {
+
+    class RawArguments{
+
+        public string EndPoint = string.Empty;
+
+        public string Query = string.Empty;
+
+        public Modee Mode= Modee.POST;
+
+        public bool Pretty = false;
+
+        public enum Modee {
+            GET,
+            POST
+        }
+
+    }
+
+}
